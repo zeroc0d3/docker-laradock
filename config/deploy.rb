78b185568c7b6938c284c7dd6f571f05e41d095e
Dwi Fahni Denni (zeroc0d3) <zeroc0d3.0912@gmail.com>
2018-04-24 07:28:01 +0700

    Add default laradock as repository deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.10.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "laradock"
+set :repo_url, "git@github.com:laradock/laradock.git"
 
 # Default branch is :master
 set :branch,    'master'
